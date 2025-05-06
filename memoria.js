f74492e97de152eb680620494048c2a1925bccde
João Flávio <168208141+Joao1771@users.noreply.github.com>
2025-05-06 11:34:35 -0300

    alpha 0.2

--- memoria.js
+++ memoria.js
@@ -2,23 +2,33 @@ function ai(){
     alert("AAAAAAAAAAAAAAA")
 }
 function rodar(){
-    const cartas = document.querySelectorAll(".carta")
+    const cartas = document.querySelectorAll(".verso")
+    let fundos = []
+    let cont = 0
     function rand(min = 0, max = 255){
         return Math.random() * (max - min) - min
     }
+    function voltarAnimacao(){
+        const body = document.querySelector("body")
+        body.classList.remove("no-animation")
+    }
+    setTimeout(voltarAnimacao,500)
+    // função responsável por colocar background, randomizar e salvar no array "fundos" as cores 
+    // escolhidas
     function pintar(){
         let cores = []
-        let cont = 0
         let corEscolhida
         while (cont<cartas.length / 2){ // enquanto for a metade do total das cartas
-            corEscolhida = `rgb(${rand()},${rand()},${rand()})`
+            corEscolhida = `rgb(${Math.floor(rand())},${Math.floor(rand())},${Math.floor(rand())})`
             cores.push(corEscolhida,corEscolhida) // coloca uma cor em cores
             cont+= 1
         }
+        cont = 0
         for(let carta of cartas){
-            corEscolhida = cores[Math.floor(rand(0,cartas.length / 2))]
-            carta.style.background = corEscolhida // coloca a cor no background de cada carta
-            // aqui precisa tirar a cor de cores para ela se repetir só duas vezes
+            corEscolhida = Math.floor(rand(0,cores.length)) //escolhe um número entre 0 e cartas.length
+            carta.style.background = cores[corEscolhida] // coloca a cor no background de cada carta
+            fundos.push(cores[corEscolhida])
+            cores.splice(cores.indexOf(cores[corEscolhida]), 1)
         }
     }
     pintar()
