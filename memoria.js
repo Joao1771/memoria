59b15807168b8b71786a2b8891b400e467db927b
João Flávio <168208141+Joao1771@users.noreply.github.com>
2025-05-09 11:22:26 -0300

    beta v0.3
    working

--- memoria.js
+++ memoria.js
@@ -1,10 +1,12 @@
-function ai(){
-    alert("AAAAAAAAAAAAAAA")
-}
 function rodar(){
-    const cartas = document.querySelectorAll(".verso")
+    const mesa = document.querySelector(".mesa")
+    const versos = document.querySelectorAll(".verso")
+    const cartas = Array.from(document.querySelectorAll(".carta"))
     let fundos = []
     let cont = 0
+    let n = 0
+    let cor = ""
+
     function rand(min = 0, max = 255){
         return Math.random() * (max - min) - min
     }
@@ -18,13 +20,13 @@ function rodar(){
     function pintar(){
         let cores = []
         let corEscolhida
-        while (cont<cartas.length / 2){ // enquanto for a metade do total das cartas
+        while (cont<versos.length / 2){ // enquanto for a metade do total das cartas
             corEscolhida = `rgb(${Math.floor(rand())},${Math.floor(rand())},${Math.floor(rand())})`
             cores.push(corEscolhida,corEscolhida) // coloca uma cor em cores
             cont+= 1
         }
         cont = 0
-        for(let carta of cartas){
+        for(let carta of versos){
             corEscolhida = Math.floor(rand(0,cores.length)) //escolhe um número entre 0 e cartas.length
             carta.style.background = cores[corEscolhida] // coloca a cor no background de cada carta
             fundos.push(cores[corEscolhida])
@@ -32,4 +34,38 @@ function rodar(){
         }
     }
     pintar()
+    // "remove" os pares de cartas iguais (igual no jogo de memória mesmo)
+    function removerCartas(){
+        for(carta of document.querySelectorAll(".girar")){
+            carta.classList.add("removida")
+        }
+    }
+
+    //gira a carta escolhida, não gira se for a mesma e limita em duas para voltarem como estavam
+    function girar(elemento){
+        let cartaEscolhida = elemento.querySelector(".verso")
+        if(!elemento.classList.contains("girar")){
+            elemento.classList.add("girar")
+            n += 1
+        }else return
+        if(n < 2){
+             cor = cartaEscolhida.style.background
+        }else{
+            if(cartaEscolhida.style.background == cor) removerCartas()
+        }
+        for(let carta of cartas){
+            if(n >= 2 && carta.classList.contains("girar")){
+                setTimeout(()=>carta.classList.remove("girar"),1000)
+            }
+        }
+        if(n >= 2) n = 0
+    }
+
+    mesa.addEventListener("click", function(e){
+        const cartaClick = e.target.closest(".carta")
+
+        if(cartaClick && cartas.includes(cartaClick)){
+            girar(cartaClick)
+        }
+    })
 }
